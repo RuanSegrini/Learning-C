fddd79ff516a4a7d909835267eadfc92fb1ff91c
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2023-05-25 14:06:08 -0300

    Create Soma.c

---
+++ EXERCICIOS-C/ESTRUTURA-SEQUENCIAL/Soma.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() { 
+
+    int X, Y, SOMA;
+
+    printf("Digite o valor de X: ");
+    scanf("%d", &X);
+    printf("Digite o valor de Y: ");
+    scanf("%d", &Y);
+
+SOMA = X + Y;
+
+printf("SOMA = %d", SOMA);
+
+
+
+    return 0;
+}
