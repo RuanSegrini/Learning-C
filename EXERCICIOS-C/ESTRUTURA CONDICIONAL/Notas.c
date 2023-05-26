47b0ea1bc848072a7d5b040a63ea76af533ef554
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2023-05-26 14:13:25 -0300

    Update Notas.c

--- EXERCICIOS-C/ESTRUTURA CONDICIONAL/Notas.c
+++ EXERCICIOS-C/ESTRUTURA CONDICIONAL/Notas.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() { 
+
+    double nota1, nota2, notaFinal;
+
+    printf("Digite a primeira nota: ");
+    scanf("%lf", &nota1);
+    printf("Digite a segunda nota: ");
+    scanf("%lf", &nota2);
+
+    notaFinal = nota1 + nota2;
+
+    if(notaFinal < 60.00){
+        printf("NOTA FINAL = %.1lf\n", notaFinal);
+        printf("REPROVADO");
+
+    }
+    else{
+        printf("NOTA FINAL = %.1lf\n", notaFinal);
+        printf("APROVADO");
+    }
+
+      return 0;
+}
