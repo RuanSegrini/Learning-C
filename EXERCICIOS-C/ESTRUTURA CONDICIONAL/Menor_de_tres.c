d76c5698aab9a636b73e1a3b260986949fda8bfa
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2023-05-26 14:33:58 -0300

    Create Menor_de_tres.c

---
+++ EXERCICIOS-C/ESTRUTURA CONDICIONAL/Menor_de_tres.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() { 
+
+int primeiro, segundo, terceiro;
+
+printf("Primeiro valor: ");
+scanf("%d", &primeiro);
+printf("Segundo valor: ");
+scanf("%d", &segundo);
+printf("Terceiro valor: ");
+scanf("%d", &terceiro);
+
+
+if(primeiro < segundo && primeiro < terceiro){
+
+printf("MENOR = %d", primeiro);
+
+}
+if(segundo < primeiro && segundo < terceiro){
+
+printf("MENOR = %d", segundo);
+
+}
+else{
+    printf("MENOR = %d",terceiro);
+}
+
+    return 0;
+}
